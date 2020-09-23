3fe1a447b649fb1cea978a3d2ee4be271d5922fa
Marta Pancaldi <marta.panc@gmail.com>
2020-09-23 20:57:39 +0100

    Add Home/Details navigator test

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,6 +3,8 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function TabOneScreen() {
   return (
@@ -13,6 +15,30 @@ export default function TabOneScreen() {
     </View>
   );
 }
+
+function HomeScreen() {
+  return (
+      <View style={styles.container}>
+          <Text style={styles.title}>Tab One</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <EditScreenInfo path="/screens/TabOneScreen.js" />
+          <HomeScreen />
+          <View style={{flex: 0, marginTop: '40px', alignItems: 'center', justifyContent: 'center'}}>
+              <Text>Home Screen</Text>
+          </View>
+      </View>
+  );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Details Screen</Text>
+        </View>
+    )
+}
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
